f62c1c36c7ad5c128a8245ad562a50619e5c0995
Anne Starley <annestarley@gmail.com>
2017-11-28 11:23:40 -0800

    Working

--- scripts.js
+++ scripts.js
@@ -4,16 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
   var colors = document.getElementsByClassName('circle')
   var currentColor = document.getElementById('current-color')
   var selectedColor = 'white'
+  var gridCells = document.getElementsByClassName('grid-cell-medium')
 
   for (var i = 0; i < 2800; i++) {
     var gridContainer = document.getElementById('grid-container')
     gridCell = document.createElement('div')
     gridCell.classList.add('grid-cell-medium')
     gridContainer.appendChild(gridCell)
-
-    // gridCell.addEventListener('click' function(e) {
-    //   e.target.style.backgroundColor = selectedColor
-    // })
   }
 
   for (var i = 0; i < colors.length; i++) {
@@ -23,5 +20,11 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   }
 
+  for (var i = 0; i < gridCells.length; i++) {
+    gridCells[i].addEventListener('click', function(e) {
+      e.target.style.backgroundColor = selectedColor
+      e.target.style.border = "none"
+    })
+  }
 
 })
